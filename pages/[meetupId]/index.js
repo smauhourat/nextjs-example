0f9bd4df7d08502b0b866f6f634b7254e5d868ee
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-09-09 18:06:52 -0300

    getsaticpaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,42 @@ function MeetupDetails() {
     );
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                },
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                },
+            },
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single Meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupDate: {
+                id: meetupId,
+                image: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Male_northern_cardinal_in_Central_Park_%2852612%29.jpg',
+                title: 'A First Meetup',
+                address: 'Some Street 5, 123132 Some City',
+                description: 'The meet description'                
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,32 @@ const DUMMY_MEETUPS = [
     },
 ];
 
-function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+function HomePage(props) {
+    return <MeetupList meetups={props.meetups} />
+}
+
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+
+//     // fetch data from API
+
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     }
+// }
+
+export async function getStaticProps() {
+    // fetch data from API
+
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        },
+        revalidate: 1
+    }
 }
 
 export default HomePage;
