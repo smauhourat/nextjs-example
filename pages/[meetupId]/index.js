dfafcf2bc91b1dfd01f56eae25e5536e567fc68e
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-09-09 18:45:01 -0300

    extras

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,7 +13,7 @@ function MeetupDetails() {
 
 export async function getStaticPaths() {
     return {
-        fallback: false,
+        fallback: true,
         paths: [
             {
                 params: {
